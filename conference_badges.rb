87e1691845acfe707a8cde8ca84d961e982c5571
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-30 00:56:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect { |n| badge_maker(n)}
+end
+
+def assign_rooms(attendees)
+  attendees.collect { |n| "Hello, #{n}! You'll be assigned to room #{attendees.index(n).to_i + 1}!" }
+end
+
+def printer(attendees)
+  attendees.collect { |n| "#{batch_badge_creator(n)} #{assign_rooms(n)}" }
+end
